20a752926ca56b7336602d850df462f51fed0c79
L Stephens <lstephens46@my.stlcc.edu>
2019-04-11 14:31:49 -0500

    finish bottles of beer pass all tests

--- BottlesOfBeer/Program.cs
+++ BottlesOfBeer/Program.cs
@@ -9,7 +9,15 @@ namespace BottlesOfBeer
             // TODO: Output "99 bottles of beer on the wall.  99 bottles of beer.  Take 1 down and pass it around, 98 bottles of beer on the wall.";
             // Count down until there are no more bottles of beer on the wall.
 
+
+            for (int bottle = 99; bottle <= 99; bottle--)
+                if (bottle <= 99 && bottle >= 1)
+                {
+                    Console.WriteLine($"{bottle} bottles of beer on the wall.  {bottle} bottles of beer.  Take 1 down and pass it around, {bottle - 1} bottles of beer on the wall.");
+                }
             Console.ReadLine();
         }
+
+
     }
 }
